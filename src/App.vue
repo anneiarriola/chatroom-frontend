65cd9c2425f03488cf4289882875819ea9a4ec16
anneiarriola <95490269+anneiarriola@users.noreply.github.com>
2023-10-09 00:45:18 -0500

    init frontend ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <welcome-chat/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeChat from './components/WelcomeChat.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
+  components: {
+    WelcomeChat,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-application {
+  font-family: "Raleway", sans-serif !important;
+  line-height: 1.5;
 }
 </style>
---
+++ src/components/WelcomeChat.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-content>
+            <v-list-item-title>Chats</v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="primary">mdi-plus</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>ChatRoom</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <h1>Welcome to the Chat!</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script>
